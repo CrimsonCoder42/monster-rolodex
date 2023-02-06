852e752436a8647751326bf0128ff49edb99ec37
CrimsonCoder42 <nostro37@gmail.com>
2023-02-06 11:30:02 -0500

    fetch from name api

--- src/App.js
+++ src/App.js
@@ -8,31 +8,30 @@ class App extends Component {
       super();
 
       this.state = {
-          monsters: [
-              {
-                  name: 'Linda'
-              },
-              {
-                  name: 'Frank'
-              },
-              {
-                  name: 'Jacky'
-              },
-              {
-                  name: 'Joe'
-              },
-          ]
-
+          monsters: [],
       };
   }
 
+  componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+          .then((response) => response.json())
+          .then((users) => this.setState(() => {
+              return {monsters: users}
+          },
+              () => {
+              console.log(this.state)
+              }))
+  }
 
-  render() {
+    render() {
     return (
         <div className="App">
-            {
-                this.state.monsters.map((monster) => {
-                    return <h1>{monster.name}</h1>
+            {this.state.monsters.map((monster) => {
+                return (
+                    <div key={monster.id}>
+                        <h1>{monster.name}</h1>
+                    </div>
+                )
                 })
             }
         </div>
