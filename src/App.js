f04f15f31188ff5cca2ee6831116743222d34e59
CrimsonCoder42 <nostro37@gmail.com>
2023-02-06 13:17:18 -0500

    close out for the day

--- src/App.js
+++ src/App.js
@@ -1,42 +1,61 @@
-import { Component } from 'react'
+import {Component} from 'react'
 
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(){
-      super();
+    constructor() {
+        super();
 
-      this.state = {
-          monsters: [],
-      };
-  }
+        this.state = {
+            monsters: [],
+            searchField: ''
+        };
+        console.log('constructor')
+    }
 
-  componentDidMount() {
-      fetch('https://jsonplaceholder.typicode.com/users')
-          .then((response) => response.json())
-          .then((users) => this.setState(() => {
-              return {monsters: users}
-          },
-              () => {
-              console.log(this.state)
-              }))
-  }
+    componentDidMount() {
+        console.log('componentDidMount')
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((users) => this.setState(() => {
+                    return {monsters: users}
+                },
+                () => {
+
+                }))
+    }
 
     render() {
-    return (
-        <div className="App">
-            {this.state.monsters.map((monster) => {
-                return (
-                    <div key={monster.id}>
-                        <h1>{monster.name}</h1>
-                    </div>
-                )
+        console.log('render')
+
+        const filteredMonsters = this.state.monsters.filter((monster) => {
+            return monster.name.toLowerCase().includes(this.state.searchField);
+        });
+
+        return (
+            <div className="App">
+                <input className='search-box'
+                       type='search'
+                       placeholder='search monsters'
+                       onChange={(e) => {
+                           const searchField = e.target.value.toLowerCase()
+                           this.setState(() => {
+                               return { searchField };
+                           });
+                       }}
+                />
+                {filteredMonsters.map((monster) => {
+                    return (
+                        <div key={monster.id}>
+                            <h1>{monster.name}</h1>
+                        </div>
+                    )
                 })
-            }
-        </div>
-    );
-  }
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
